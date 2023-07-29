39f55d7a4eb044a7049fa1eb3ad41f61b4a6f30d
Jason Thorpe <thorpej@me.com>
2023-07-29 09:26:10 -0700

    Add memzero8() and memzero16() subroutines.

---
+++ lib/memzero.s
@@ -0,0 +1,70 @@
+;       
+; Copyright (c) 2022, 2023 Jason R. Thorpe.
+; All rights reserved.
+;       
+; Redistribution and use in source and binary forms, with or without
+; modification, are permitted provided that the following conditions
+; are met:
+; 1. Redistributions of source code must retain the above copyright
+;    notice, this list of conditions and the following disclaimer.
+; 2. Redistributions in binary form must reproduce the above copyright
+;    notice, this list of conditions and the following disclaimer in the
+;    documentation and/or other materials provided with the distribution.
+;
+; THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR
+; IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+; OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+; IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT,
+; INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+; BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED
+; AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+; OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+; OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+; SUCH DAMAGE.
+;       
+
+;
+; memzero8
+;	Set memory to zero, 8-bit length.
+;
+; Arguments --
+;	X - destination
+;	A - byte count (0 == 256)
+;
+; Returns --
+;	None.
+;
+; Clobbers --
+;	None.
+;
+memzero8
+	pshs	A,X		; Save registers
+1	clr	,X+		; *dst++ = 0
+	deca			; A--
+	bne	1B		; Keep going if more bytes left
+	puls	A,X,PC		; Restore and return
+
+;
+; memzero16
+;	Set memory to zero, 16-bit length.
+;
+; Arguments --
+;	X - destination
+;	D - byte count (0 == 0)
+;
+; Returns --
+;	None.
+;
+; Clobbers --
+;	None.
+;
+memzero16
+	pshs	D,X,Y		; Save registers
+	leay	D,X		; Compute ending address
+	pshs	Y		; push it onto the stack
+1	clr	,X+		; *dst++ = 0
+	cmpx	,S		; X == ending address?
+	bne	1B		; Nope, keep going
+	leas	2,S		; pop ending address off stack
+	puls	D,X,Y,PC	; Restore and return
