23f7d691f570e0b7a0d216dc5097348cadb01306
Jason Thorpe <thorpej@me.com>
2023-08-10 22:32:17 -0700

    Put the pgmon help in banked ROM 0.

--- banked-rom0/pg09-brom0.s
+++ banked-rom0/pg09-brom0.s
@@ -44,13 +44,120 @@
 ; Banked call jump table.  The order of this table defines the ABI
 ; of the module in this ROM bank.
 ;
+	BCall cmd_help
 	BCall cmd_oink
 
 ;
 ; Code goes here.
 ;
 
+	include "../lib/parseeol.s"
+	include "../lib/parsews.s"
+	include "../lib/parsetbl.s"
 	include	"../lib/puts.s"
+	include "../lib/toupper.s"
+
+;
+; cmd_help
+;	Get help.
+;
+cmd_help
+	; First check for command help.
+	jsr	parseeol		; if we are at EOL
+	bne	cmd_help_generic	; then generic help it is.
+
+	; Whitespace already consumed by parseeol().  Now look up
+	; the command in the help table.
+	ldy	#monitor_helptab	; Y = help table
+	jsr	parsetbl_lookup		; A = command index
+	asla				; index -> offset
+	ldy	#monitor_helpjmptab	; Y = help jump table
+	jmp	[A,Y]			; go get the help
+
+monitor_helptab
+	fcc	'@'+$80			; access memory
+	fcc	'J'+$80			; jump to address
+	fcc	"REG",'S'+$80		; registers
+	fcc	'R'+$80			; print / set register
+	fcc	"LOAD",'S'+$80		; load S-Records
+	fcc	"ADDR",'S'+$80		; symbolic addresses
+	fcc	0
+
+monitor_helpjmptab
+	fdb	cmd_help_access_mem
+	fdb	cmd_help_jump
+	fdb	cmd_help_regs
+	fdb	cmd_help_reg
+	fdb	cmd_help_loads
+	fdb	cmd_help_addrs
+	fdb	cmd_help_generic
+
+cmd_help_generic
+	jsr	iputs
+	fcc	"Available commands:\r\n"
+	fcc	"@     - access memory\r\n"
+	fcc	"J     - jump to address\r\n"
+	fcc	"R     - print / set register\r\n"
+	fcc	"LOADS - load S-Records\r\n"
+	fcc	"?     - help\r\n"
+	fcn	"Use '? <cmd>' for additional help.\r\n"
+	rts
+
+cmd_help_access_mem
+	jsr	iputs
+	fcc	"@addr               - print 1 byte\r\n"
+	fcc	"@                   - print 1 byte at next address\r\n"
+	fcc	"@addr,len           - print len bytes\r\n"
+	fcc	"@,len               - print len bytes at next address\r\n"
+	fcc	"@addr val [val ...] - set bytes starting at address\r\n"
+	fcc	"@ val [val ...]     - set bytes starting at next address\r\n"
+	fcc	"@addr,len val       - set len bytes at address to value\r\n"
+	fcc	"@,len val           - set len bytes at next address to value\r\n"
+	fcn	"Use '? addrs' for a list of symbolic addresses.\r\n"
+	rts
+
+cmd_help_jump
+	jsr	iputs
+	fcc	"J      - jump to entry point of loaded program\r\n"
+	fcn	"J addr - jump to specified address\r\n"
+	rts
+
+cmd_help_reg
+	jsr	iputs
+	fcc	"R         - print all registers\r\n"
+	fcc	"R reg     - print single register\r\n"
+	fcc	"R reg val - set register to value\r\n"
+	fcn	"Use '? regs' for a list of registers.\r\n"
+	rts
+
+cmd_help_loads
+	jsr	iputs
+	fcc	"LOADS - load S-Records\r\n"
+	fcc	"S19-style S-Records are loaded from the console.\r\n"
+	fcc	"Use 'J' to start loaded program.\r\n"
+	fcn	"Use CTRL-C to abort loading.\r\n"
+	rts
+
+cmd_help_addrs
+	jsr	iputs
+	fcc	"Available symbolic addresses:\r\n"
+	fcc	"  ROM_BANK_REG\r\n"
+	fcc	"  LBRAM_BANK_REG\r\n"
+	fcc	"  HBRAM_BANK_REG\r\n"
+	fcc	"  CLOCK_SPEED_REG\r\n"
+	fcc	"  LBRAM_START\r\n"
+	fcc	"  HBRAM_START\r\n"
+	fcc	"  BROM_START\r\n"
+	fcc	0
+	rts
+
+cmd_help_regs
+	jsr	iputs
+	fcc	"8-bit registers:\r\n"
+	fcc	"  A B CCR DP\r\n\r\n"
+	fcc	"16-bit registers:\r\n"
+	fcn	"  D X Y U PC\r\n"
+	rts
 
 ;
 ; cmd_oink
