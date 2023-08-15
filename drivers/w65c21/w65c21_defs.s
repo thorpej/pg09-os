bb924c37ca0923e40ef31677536f5ff21f9fc377
Jason Thorpe <thorpej@me.com>
2023-08-15 10:22:12 -0700

    Clean up PIA register defs.

--- drivers/w65c21/w65c21_defs.s
+++ drivers/w65c21/w65c21_defs.s
@@ -44,19 +44,17 @@ PIA_REG_CRB	equ	3		; Control Register B
 ; Control Register definitions
 ; IRQ1 --> CA1/CB1 pins
 ; IRQ2 --> CA2/CB2 pins
-PIA_CRx_Cx1_NE_DIS	equ	(0 << 0) ; IRQ1 negative edge (disabled)
-PIA_CRx_Cx1_NE		equ	(1 << 0) ; IRQ1 negative edge (enabled)
-PIA_CRx_Cx1_PE_DIS	equ	(2 << 0) ; IRQ1 positive edge (disabled)
-PIA_CRx_Cx1_PE		equ	(3 << 0) ; IRQ1 positive edge (enabled)
+PIA_CRx_Cx1_IRQ_NE	equ	1	 ; IRQ1 enabled, negative edge
+PIA_CRx_Cx1_IRQ_PE	equ	3	 ; IRQ1 enabled, positive edge
+PIA_CRx_Cx1_MASK	equ	3	 ; (mask of relevant bits)
 PIA_CRx_DDR_PI		equ	(1 << 2) ; 0 -> DDR access, 1 -> Peripheral
-PIA_CRx_Cx2_NE_DIS	equ	(0 << 3) ; IRQ2 negative edge (disabled)
-PIA_CRx_Cx2_NE		equ	(1 << 3) ; IRQ2 negative edge (enabled)
-PIA_CRx_Cx2_PE_DIS	equ	(2 << 3) ; IRQ2 positive edge (disabled)
-PIA_CRx_Cx2_PE		equ	(3 << 3) ; IRQ2 positive edge (enabled)
-PIA_CRx_Cx2_HS_READ	equ	(4 << 3) ; Cx2 handshake on read
-PIA_CRx_Cx2_PO		equ	(5 << 3) ; Cx2 pulse output
+PIA_CRx_Cx2_IRQ_NE	equ	(1 << 3) ; IRQ2 negative edge (enabled)
+PIA_CRx_Cx2_IRQ_PE	equ	(3 << 3) ; IRQ2 positive edge (enabled)
+PIA_CRx_Cx2_HS		equ	(4 << 3) ; Cx2 handshake
+PIA_CRx_Cx2_PULSE	equ	(5 << 3) ; Cx2 pulse
 PIA_CRx_Cx2_MAN_LOW	equ	(6 << 3) ; Cx2 manual output low
 PIA_CRx_Cx2_MAN_HIGH	equ	(7 << 3) ; Cx2 manual output high
+PIA_CRx_Cx2_MASK	equ	(7 << 3) ; (mask of relevant bits)
 PIA_CRx_IRQ2		equ	(1 << 6) ; IRQ2 active
 PIA_CRx_IRQ1		equ	(1 << 7) ; IRQ1 active
 
