fa2b3bb3b96fb9092889c1550aa88fd85bb1c1c9
Jason Thorpe <thorpej@me.com>
2023-10-03 11:40:40 -0700

    Loop so that large transfers fit within the NHACP max-payload limit.

--- nhacp/file-nhacp.s
+++ nhacp/file-nhacp.s
@@ -39,6 +39,16 @@ fcb_nhacp_ctx		equ	(fcb_opaque + 0)	; 2 bytes
 fcb_nhacp_fd		equ	(fcb_nhacp_ctx + 2)	; 1 byte
 
 ;
+; NHACP specifies a max payload of 8192 bytes, so we may need to loop
+; around requests that are longer than that.  We use temporary space
+; in the FCB to track the work.
+;
+fcb_nhacp_ptr		equ	(fcb_nhacp_fd + 1)	; 2 bytes
+fcb_nhacp_resid		equ	(fcb_nhacp_ptr + 2)	; 2 bytes
+fcb_nhacp_offset	equ	(fcb_nhacp_resid + 2)	; 4 bytes
+fcb_nhacp_actual	equ	(fcb_nhacp_offset + 4)	; 2 bytes
+
+;
 ; file_nhacp_open --
 ;	file_open routine using NHACP.
 ;
@@ -148,6 +158,67 @@ file_nhacp_io_jmptab_end
 file_nhacp_io_jmptab_size	equ	(file_nhacp_io_jmptab_end-file_nhacp_io_jmptab)
 
 ;
+; file_nhacp_io_advance
+;	Advance the I/O pointer in the FCB.
+;
+; Arguments --
+;	D - byte count
+;	Y - pointer to FCB
+;
+; Returns --
+;	None.
+;
+; Clobbers --
+;	None.
+;
+file_nhacp_io_advance
+	pshs	D		; stash byte count on stack
+
+	; First update residual count.  If we get to 0, we can
+	; skip updating the data pointer, but we do need to
+	; update the actual count.
+	ldd	fcb_nhacp_resid,Y
+	subd	,S
+	std	fcb_nhacp_resid,Y
+	beq	1F
+
+	ldd	fcb_nhacp_ptr,Y
+	addd	,S
+	std	fcb_nhacp_ptr,Y
+
+1	ldd	fcb_nhacp_actual,Y
+	addd	,S
+	std	fcb_nhacp_actual,Y
+
+	puls	D,PC		; restore and return
+
+;
+; file_nhacp_io_advance_offset
+;	Advance the I/O pointer in the FCB, including the offset.
+;
+; Arguments --
+;	D - byte count
+;	Y - pointer to FCB
+;
+; Returns --
+;	None.
+;
+; Clobbers --
+;	D
+;
+file_nhacp_io_advance_offset
+	bsr	file_nhacp_io_advance
+
+	; Add D to the 32-bit number at fcb_nhacp_offset,Y.
+	addd	fcb_nhacp_offset+2,Y
+	std	fcb_nhacp_offset+2,Y
+	bcc	1F
+	ldd	#1
+	addd	fcb_nhacp_offset,Y
+	std	fcb_nhacp_offset,Y
+1	rts
+
+;
 ; file_nhacp_io --
 ;	file_io routine using NHACP.
 ;
@@ -175,6 +246,21 @@ file_nhacp_io
 	ldy	fio_fcb,X	; Y = FCB
 	ldu	fcb_nhacp_ctx,Y	; U = NHACP context
 
+	;
+	; Before we dispatch to the real I/O routine, stash
+	; the arguments in the FCB.
+	;
+	ldd	fio_data,X
+	std	fcb_nhacp_ptr,Y
+	ldd	fio_length,X
+	std	fcb_nhacp_resid,Y
+	ldd	fio_offset,X
+	std	fcb_nhacp_offset,Y
+	ldd	fio_offset+2,X
+	std	fcb_nhacp_offset+2,Y
+	clr	fcb_nhacp_actual,Y
+	clr	fcb_nhacp_actual+1,Y
+
 	pshs	Y		; stash FCB on stack
 	;
 	; 9,S
@@ -194,29 +280,29 @@ file_nhacp_io
 	cmpa	#file_nhacp_io_jmptab_size
 	bhs	file_nhacp_io_einval
 
-	; We're going to lose track of the FCB when we do the
-	; jump, but the only thing we really need from it is
-	; the file descriptor, so just stash that in the request
-	; structure now.
+	; Each of the ops in question has a file descriptor at
+	; the same slot in the request, so just stash it now.
 	ldb	fcb_nhacp_fd,Y	; B = file descriptor
 	stb	nhctx_req_args,U ; stash fdesc in request
 
 	; Jump to the handler.
-	ldy	#file_nhacp_io_jmptab
-	jsr	[A,Y]
+	ldx	#file_nhacp_io_jmptab
+	jsr	[A,X]
 
 file_nhacp_io_read
 	nhacp_req_init "FILE_READ"
 
-	ldd	#0		; flags
-	std	nhctx_req_args+1
-
-	ldd	fio_length,X
-	beq	file_nhacp_io_done ; done early if length == 0
-	stb	nhctx_req_args+3
+	ldd	fcb_nhacp_resid,Y
+	beq	file_nhacp_io_done ; done if resid == 0
+	cmpd	#NHACP_MAX_PAYLOAD
+	bls	1F
+	ldd	#NHACP_MAX_PAYLOAD ; clamp!
+1	stb	nhctx_req_args+3
 	sta	nhctx_req_args+4
 
-file_nhacp_io_read_common
+	ldd	#0		; flags
+	std	nhctx_req_args+1,U
+
 	jsr	nhacp_req_send
 
 	; Receive the reply.
@@ -238,55 +324,42 @@ file_nhacp_io_read_common
 	jsr	nhacp_get_reply_byte
 	puls	B		; D now contains returned length
 
-	; Recover arg pointer and read the data into the caller's
-	; buffer.
-	ldx	4,S
-	std	fio_actual,X
-	beq	1F		; D == 0, no data to copy in.
-	ldx	fio_data,X
-	jsr	nhacp_copyin
-1	bra	file_nhacp_io_done
+	cmpd	#0		; actual == 0?
+	beq	file_nhacp_io_done
+
+	ldy	,S		; recover FCB
+	ldx	fcb_nhacp_ptr,Y	; X = current data pointer
+	pshs	D,Y		; preserve D,Y
+	jsr	nhacp_copyin	; get data from interface
+	puls	D,Y		; restore D,Y
+
+	bsr	file_nhacp_io_advance
+	bra	file_nhacp_io_read
 
 file_nhacp_io_pread
 	nhacp_req_init "STORAGE_GET"
+
+	ldd	fcb_nhacp_resid,Y
+	beq	file_nhacp_io_done ; done if resid == 0
+	cmpd	#NHACP_MAX_PAYLOAD
+	bls	1F
+	ldd	#NHACP_MAX_PAYLOAD ; clamp!
+1	stb	nhctx_req_args+5
+	sta	nhctx_req_args+6
 
 	; We have a 32-bit big-endian number that needs to
 	; be stored in a 32-bit little-endian field.
 	;
 	; 0 1 2 3	0 1 2 3
 	; 4 3 2 1	1 2 3 4
-	ldd	fio_offset,X	; A = 4, B = 3
+	ldd	fcb_nhacp_offset,Y	; A = 4, B = 3
 	sta	nhctx_req_args+4
 	stb	nhctx_req_args+3
-	ldd	fio_offset+2,X	; A = 2, B = 1
+	ldd	fcb_nhacp_offset+2,Y	; A = 2, B = 1
 	sta	nhctx_req_args+2
 	stb	nhctx_req_args+1
 
-	ldd	fio_length,X
-	beq	file_nhacp_io_done ; done early if length == 0
-	stb	nhctx_req_args+5
-	sta	nhctx_req_args+6
-
-	bra	file_nhacp_io_read_common
-
-file_nhacp_io_write
-	nhacp_req_init "FILE_WRITE"
-
-	ldd	#0		; flags
-	std	nhctx_req_args+1
-
-	ldd	fio_length,X
-	beq	file_nhacp_io_done ; done early if length == 0
-	stb	nhctx_req_args+3
-	sta	nhctx_req_args+4
-
-file_nhacp_io_write_common
 	jsr	nhacp_req_send
-
-	; send the data
-	ldd	fio_length,X
-	ldx	fio_data,X
-	jsr	nhacp_copyout
 
 	; Receive the reply.
 	jsr	nhacp_get_reply_hdr
@@ -297,42 +370,130 @@ file_nhacp_io_write_common
 	cmpa	#NHACP_RESP_ERROR
 	beq	file_nhacp_io_error_reply
 
+	; Unknown error if not DATA_BUFFER.
+	cmpa	#NHACP_RESP_DATA_BUFFER
+	bne	file_nhacp_io_eio
+
+	; Get returned length into D.
+	jsr	nhacp_get_reply_byte
+	pshs	A		; LSB
+	jsr	nhacp_get_reply_byte
+	puls	B		; D now contains returned length
+
+	cmpd	#0		; actual == 0?
+	beq	file_nhacp_io_done
+
+	ldy	,S		; recover FCB
+	ldx	fcb_nhacp_ptr,Y	; X = current data pointer
+	pshs	D,Y		; preserve D,Y
+	jsr	nhacp_copyin	; get data from interface
+	puls	D,Y		; restore D,Y
+
+	bsr	file_nhacp_io_advance_offset
+	bra	file_nhacp_io_pread
+
+file_nhacp_io_write
+	nhacp_req_init "FILE_WRITE"
+
+	ldd	fcb_nhacp_resid,Y
+	beq	file_nhacp_io_done ; done if resid == 0
+	cmpd	#NHACP_MAX_PAYLOAD
+	bls	1F
+	ldd	#NHACP_MAX_PAYLOAD ; clamp!
+1	stb	nhctx_req_args+3
+	sta	nhctx_req_args+4
+	pshs	D		; stash length on stack temporarily
+
+	ldd	#0		; flags
+	std	nhctx_req_args+1,U
+
+	jsr	nhacp_req_send
+
+	; send the data
+	ldd	,S		; get length from stack
+	ldx	fcb_nhacp_ptr,Y
+	jsr	nhacp_copyout
+
+	; Receive the reply.
+	jsr	nhacp_get_reply_hdr
+	beq	file_nhacp_write_eio
+
+	; Check for ERROR reply.
+	lda	nhctx_reply_type,U
+	cmpa	#NHACP_RESP_ERROR
+	beq	file_nhacp_write_error_reply
+
 	; Unknown error if not OK.
 	cmpa	#NHACP_RESP_OK
-	bne	file_nhacp_io_eio
+	bne	file_nhacp_write_eio
 
 	; XXX NHACP's FILE-WRITE does not return the amount
 	; of data actually written.  We have to assume it is
 	; the full amount.
+	puls	D		; pop length from stack
 
-	; Recover arg pointer and read the data into the caller's
-	; buffer.
-	ldx	4,S
-	ldd	fio_length,X
-	std	fio_actual,X
-	bra	file_nhacp_io_done
+	bsr	file_nhacp_io_advance
+	bra	file_nhacp_io_write
+
+file_nhacp_write_eio
+	leas	1,S		; pop length off stack
+	bra	file_nhacp_io_eio
+
+file_nhacp_write_error_reply
+	leas	1,S		; pop length off stack
+	bra	file_nhacp_io_error_reply
 
 file_nhacp_io_pwrite
 	nhacp_req_init "STORAGE_PUT"
+
+	ldd	fcb_nhacp_resid,Y
+	beq	file_nhacp_io_done ; done if resid == 0
+	cmpd	#NHACP_MAX_PAYLOAD
+	bls	1F
+	ldd	#NHACP_MAX_PAYLOAD ; clamp!
+1	stb	nhctx_req_args+5
+	sta	nhctx_req_args+6
+	pshs	D		; stash length on stack temporarily
 
 	; We have a 32-bit big-endian number that needs to
 	; be stored in a 32-bit little-endian field.
 	;
 	; 0 1 2 3	0 1 2 3
 	; 4 3 2 1	1 2 3 4
-	ldd	fio_offset,X	; A = 4, B = 3
+	ldd	fcb_nhacp_offset,Y	; A = 4, B = 3
 	sta	nhctx_req_args+4
 	stb	nhctx_req_args+3
-	ldd	fio_offset+2,X	; A = 2, B = 1
+	ldd	fcb_nhacp_offset+2,Y	; A = 2, B = 1
 	sta	nhctx_req_args+2
 	stb	nhctx_req_args+1
 
-	ldd	fio_length,X
-	beq	file_nhacp_io_done ; done early if length == 0
-	stb	nhctx_req_args+5
-	sta	nhctx_req_args+6
+	jsr	nhacp_req_send
 
-	bra	file_nhacp_io_write_common
+	; send the data
+	ldd	,S		; get length from stack
+	ldx	fcb_nhacp_ptr,Y
+	jsr	nhacp_copyout
+
+	; Receive the reply.
+	jsr	nhacp_get_reply_hdr
+	beq	file_nhacp_write_eio
+
+	; Check for ERROR reply.
+	lda	nhctx_reply_type,U
+	cmpa	#NHACP_RESP_ERROR
+	beq	file_nhacp_write_error_reply
+
+	; Unknown error if not OK.
+	cmpa	#NHACP_RESP_OK
+	bne	file_nhacp_write_eio
+
+	; XXX NHACP's FILE-WRITE does not return the amount
+	; of data actually written.  We have to assume it is
+	; the full amount.
+	puls	D		; pop length from stack
+
+	bsr	file_nhacp_io_advance_offset
+	bra	file_nhacp_io_pwrite
 
 file_nhacp_io_seek
 	bra	file_nhacp_io_enotsup
