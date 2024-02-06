252986ab65369d59757b8baa61fa19e32b923b4b
Jason Thorpe <thorpej@me.com>
2024-02-06 07:12:07 -0800

    Add a strtoupper() library routine.  It just performs toupper() on a string.

---
+++ lib/strtoupper.s
@@ -0,0 +1,47 @@
+;       
+; Copyright (c) 2023 Jason R. Thorpe.
+; All rights reserved.
+;       
+; Redistribution and use in source and binary forms, with or without
+; modification, are permitted provided that the following conditions
+; are met:
+; 1. Redistributions of source code must retain the above copyright
+;    notice, this list of conditions and the following disclaimer.
+; 2. Redistributions in binary form must reproduce the above copyright
+;    notice, this list of conditions and the following disclaimer in the
+;    documentation and/or other materials provided with the distribution.
+;
+; THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR
+; IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+; OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+; IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT,
+; INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+; BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED
+; AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+; OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+; OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+; SUCH DAMAGE.
+;       
+
+;
+; strtoupper
+;	Apply toupper() to a NUL-terminated string.
+;
+; Arguments --
+;	X - the string
+;
+; Returns --
+;	None.
+;
+; Clobbers --
+;	None.
+;
+strtoupper
+	pshs	A,X		; Save registers
+1	lda	,X		; A = *X
+	beq	99F		; NUL -> all done
+	bsr	toupper		; A = toupper(A)
+	sta	,X+		; update string, advance pointer
+	bra	1B
+99	puls	A,X,PC		; restore and return
